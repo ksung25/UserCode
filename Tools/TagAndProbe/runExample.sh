8adc626643b00708d01150d5661ccd91a90f14fe
ksung <>
2012-04-03 20:27:09 +0000

    update command line arguments

--- Tools/TagAndProbe/runExample.sh
+++ Tools/TagAndProbe/runExample.sh
@@ -13,7 +13,7 @@ root -l -q plotEff.C+\(\"example.bins\",0,0,0,0,\"${NTUPLEDIR}/zee_madgraph_prob
 #
 # Electron selection efficiencies (for H->ZZ->2l2v) in Run2011A data
 #
-root -l -q plotEff.C+\(\"example.bins\",2,1,2,2,\"${NTUPLEDIR}/zee_data_probes.root\",\"Data\",\"png\",1,1,0,\"${NTUPLEDIR}/zee_madgraph_probes.root\"\)
+root -l -q plotEff.C+\(\"example.bins\",2,1,2,2,\"${NTUPLEDIR}/zee_data_probes.root\",\"Data\",\"png\",1,-1,0,\"${NTUPLEDIR}/zee_madgraph_probes.root\"\)
 
 
 rm *.so *.d
