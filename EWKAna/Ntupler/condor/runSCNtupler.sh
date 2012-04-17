b4f314ad430eab90dd41d08c51421bb1e43de42c
ksung <>
2012-04-17 15:29:31 +0000

    first commit

---
+++ EWKAna/Ntupler/condor/runSCNtupler.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+FILESET=$1
+DATASET=$2
+BOOK=$3
+CATALOG=$4
+ISDATA=$5
+USEGEN=$6
+FSRMODE=$7
+NEVENTS=$8
+SKIPHLTFAIL=$9
+
+OUTPUTDIR=/data/blue/ksung/EWKAna/bacon/$DATASET
+SCRAMDIR=/home/ksung/releases/CMSSW_5_2_3/src
+RUNDIR=$SCRAMDIR/EWKAna/Ntupler/macros
+WORKDIR=`pwd`
+export KRB5CCNAME=/home/ksung/.krb5/ticket
+
+cp $RUNDIR/runSCNtupler.C ./
+cp $RUNDIR/rootlogon.C ./
+cp $RUNDIR/START50_V15_*_AK5PF.txt ./
+cp $RUNDIR/Subdet*BDTG.weights.xml ./
+
+cd $SCRAMDIR
+SCRAM_ARCH=slc5_amd64_gcc462
+eval `scramv1 runtime -sh`
+cd $WORKDIR
+
+echo `hostname`
+echo "Running runSCNtupler.C $FILESET $DATASET $BOOK $CATALOG $ISDATA $USEGEN $FSRMODE $NEVENTS $SKIPHLTFAIL"
+echo "Working in $WORKDIR"
+ls -l
+echo "   Using $CMSSW_BASE"
+
+root -b -l -q ./rootlogon.C runSCNtupler.C+\(\"${FILESET}\",\"${DATASET}\",\"${BOOK}\",\"${CATALOG}\",${ISDATA},${USEGEN},${FSRMODE},${NEVENTS},${SKIPHLTFAIL}\)
+
+mkdir -p $OUTPUTDIR
+mv ${DATASET}_${FILESET}_scntuple.root* $OUTPUTDIR/
+
+rm -vf runSCNtupler*
+rm -vf rootlogon*
+rm -vf START50_V15_*_AK5PF.txt
+rm -vf Subdet*BDTG.weights.xml
+
+exit 0
