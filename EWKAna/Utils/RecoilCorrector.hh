eef179562126919efdbaadde33139696cd6764e9
KSUNG <>
2012-05-10 08:40:46 +0000

    implementation update

--- EWKAna/Utils/RecoilCorrector.hh
+++ EWKAna/Utils/RecoilCorrector.hh
@@ -105,7 +105,7 @@ Double_t dSigma(const TF1 *fcn, const Double_t x, const TFitResultPtr fs) {
 RecoilCorrector::RecoilCorrector(TString fname, Int_t seed)
 {
   gRandom->SetSeed(seed);
-/*
+
   TFile infile(fname);
 
   fcnPFu1mean  = (TF1*)(infile.Get("fcnPFu1mean")->Clone("fcnPFu1mean"));
@@ -172,12 +172,10 @@ RecoilCorrector::RecoilCorrector(TString fname, Int_t seed)
   fitresPFu2sigma2  = (TFitResult*)(infile.Get("fitresPFu2sigma2")->Clone("fitresPFu2sigma2"));
     
   infile.Close();
-*/
 }
 
 RecoilCorrector::~RecoilCorrector()
 {
-/*
   delete fcnPFu1mean;
   delete fcnPFu1sigma1;
   delete fcnPFu1sigma2;
@@ -198,7 +196,6 @@ RecoilCorrector::~RecoilCorrector()
   delete fitresPFu2sigma1;
   delete fitresPFu2sigma2;
   delete fitresPFu2sigma0;
-*/
 }
 
 void RecoilCorrector::Correct(Double_t &pfmet, Double_t &pfmetphi,
@@ -206,92 +203,45 @@ void RecoilCorrector::Correct(Double_t &pfmet, Double_t &pfmetphi,
 			      Double_t lepPt,  Double_t lepPhi,
 			      Int_t nsigma
 ) {
-/*
   //
   // Model for PF u1
   //
-  Double_t pfu1mean   = fcnPFu1mean->Eval(inPt);
-  Double_t pfu1sigma1 = fcnPFu1sigma1->Eval(inPt);
-  Double_t pfu1sigma2 = fcnPFu1sigma2->Eval(inPt);
-  Double_t pfu1sigma0 = fcnPFu1sigma0->Eval(inPt);
+  Double_t pfu1mean   = fcnPFu1mean->Eval(genWPt);
+  Double_t pfu1sigma1 = fcnPFu1sigma1->Eval(genWPt);
+  Double_t pfu1sigma2 = fcnPFu1sigma2->Eval(genWPt);
+  Double_t pfu1sigma0 = fcnPFu1sigma0->Eval(genWPt);
   if(nsigma!=0) {
-    //pfu1mean   += nsigma*dMean(fcnPFu1mean,inPt,fitresPFu1mean);
-    pfu1sigma1 += nsigma*dSigma(fcnPFu1sigma1,inPt,fitresPFu1sigma1);
-    pfu1sigma2 += nsigma*dSigma(fcnPFu1sigma2,inPt,fitresPFu1sigma2);
-    pfu1sigma0 += nsigma*dSigma(fcnPFu1sigma0,inPt,fitresPFu1sigma0);
+    //pfu1mean   += nsigma*dMean(fcnPFu1mean,genWPt,fitresPFu1mean);
+    pfu1sigma1 += nsigma*dSigma(fcnPFu1sigma1,genWPt,fitresPFu1sigma1);
+    pfu1sigma2 += nsigma*dSigma(fcnPFu1sigma2,genWPt,fitresPFu1sigma2);
+    pfu1sigma0 += nsigma*dSigma(fcnPFu1sigma0,genWPt,fitresPFu1sigma0);
   }
   Double_t pfu1frac2  = (pfu1sigma0 - pfu1sigma1)/(pfu1sigma2 - pfu1sigma1);
   
   //
   // Model for PF u2
   //
-  Double_t pfu2mean   = fcnPFu2mean->Eval(inPt);
-  Double_t pfu2sigma1 = fcnPFu2sigma1->Eval(inPt);
-  Double_t pfu2sigma2 = fcnPFu2sigma2->Eval(inPt);
-  Double_t pfu2sigma0 = fcnPFu2sigma0->Eval(inPt);
+  Double_t pfu2mean   = fcnPFu2mean->Eval(genWPt);
+  Double_t pfu2sigma1 = fcnPFu2sigma1->Eval(genWPt);
+  Double_t pfu2sigma2 = fcnPFu2sigma2->Eval(genWPt);
+  Double_t pfu2sigma0 = fcnPFu2sigma0->Eval(genWPt);
   if(nsigma!=0) {
-    //pfu2mean   += nsigma*dMean(fcnPFu2mean,inPt,fitresPFu2mean);
-    pfu2sigma1 += nsigma*dSigma(fcnPFu2sigma1,inPt,fitresPFu2sigma1);
-    pfu2sigma2 += nsigma*dSigma(fcnPFu2sigma2,inPt,fitresPFu2sigma2);
-    pfu2sigma0 += nsigma*dSigma(fcnPFu2sigma0,inPt,fitresPFu2sigma0);
+    //pfu2mean   += nsigma*dMean(fcnPFu2mean,genWPt,fitresPFu2mean);
+    pfu2sigma1 += nsigma*dSigma(fcnPFu2sigma1,genWPt,fitresPFu2sigma1);
+    pfu2sigma2 += nsigma*dSigma(fcnPFu2sigma2,genWPt,fitresPFu2sigma2);
+    pfu2sigma0 += nsigma*dSigma(fcnPFu2sigma0,genWPt,fitresPFu2sigma0);
   }
   Double_t pfu2frac2  = (pfu2sigma0 - pfu2sigma1)/(pfu2sigma2 - pfu2sigma1);
 
+  
   Double_t z1 = gRandom->Gaus(0,1);
   Double_t z2 = gRandom->Gaus(0,1);
-  Double_t p1 = gRandom->Gaus(0,1);
-  Double_t p2 = gRandom->Gaus(0,1);
-*/
-/*
-  Double_t pfn1  = sqrt(1.0 + pfc*pfc + u1c*u1c + sqrt(TMath::Pi())*(pfc+u1c));
-  Double_t pfn2  = sqrt(1.0 + pfc*pfc + u2c*u2c + sqrt(TMath::Pi())*(pfc+u2c));
 
-  Double_t pfu1 = (gRandom->Uniform(0,1) < pfu1frac2) ? 
-                  (1.+pfc*fabs(z2)+u1c*fabs(p1))/pfn1*pfu1sigma2*z1+pfu1mean : 
-		  (1.+pfc*fabs(z2)+u1c*fabs(p1))/pfn1*pfu1sigma1*z1+pfu1mean;
-  
-  Double_t pfu2 = (gRandom->Uniform(0,1) < pfu2frac2) ? 
-                  (1.+pfc*fabs(z1)+u2c*fabs(p2))/pfn2*pfu2sigma2*z2+pfu2mean : 
-		  (1.+pfc*fabs(z1)+u2c*fabs(p2))/pfn2*pfu2sigma1*z2+pfu2mean;
-
-  TVector2 vpfmet(-(dilepPt+pfu1)*cos(dilepPhi)+pfu2*sin(dilepPhi), -(dilepPt+pfu1)*sin(dilepPhi)-pfu2*cos(dilepPhi));  
-  pfmet    = vpfmet.Mod();
-  pfmetphi = TVector2::Phi_mpi_pi(vpfmet.Phi());  
-//*/
-/*  
-  Double_t pfc1  = hCorrPFu2u1->GetBinContent(ibin);
-  Double_t pfc2  = hCorrPFu1u2->GetBinContent(ibin);
-  
-  Double_t pfn1  = sqrt(1.0 + pfc1*pfc1 + u1c*u1c + sqrt(TMath::Pi())*(pfc1+u1c));
-  Double_t pfn2  = sqrt(1.0 + pfc2*pfc2 + u2c*u2c + sqrt(TMath::Pi())*(pfc2+u2c));
-  
-  Double_t pfu1 = (gRandom->Uniform(0,1) < pfu1frac2) ? 
-                  (1.+pfc1*fabs(z2)+u1c*fabs(p1))/pfn1*pfu1sigma2*z1+pfu1mean : 
-		  (1.+pfc1*fabs(z2)+u1c*fabs(p1))/pfn1*pfu1sigma1*z1+pfu1mean;
-  
-  Double_t pfu2 = (gRandom->Uniform(0,1) < pfu2frac2) ? 
-                  (1.+pfc2*fabs(z1)+u2c*fabs(p2))/pfn2*pfu2sigma2*z2+pfu2mean : 
-		  (1.+pfc2*fabs(z1)+u2c*fabs(p2))/pfn2*pfu2sigma1*z2+pfu2mean;
-
-  TVector2 vpfmet(-(dilepPt+pfu1)*cos(dilepPhi)+pfu2*sin(dilepPhi), -(dilepPt+pfu1)*sin(dilepPhi)-pfu2*cos(dilepPhi));  
-  pfmet    = vpfmet.Mod();
-  pfmetphi = TVector2::Phi_mpi_pi(vpfmet.Phi());   
-//*/
-/*  
-  //
-  // Get PF-recoil
-  // 
-  Double_t pfc1 = hCorrPFu2u1->GetBinContent(ibin);
-  Double_t pfn1 = sqrt(1.0 + pfc1*pfc1 + sqrt(TMath::Pi())*pfc1);
-  Double_t pfc2 = hCorrPFu1u2->GetBinContent(ibin);
-  Double_t pfn2 = sqrt(1.0 + pfc2*pfc2 + sqrt(TMath::Pi())*pfc2);
-  Double_t pfu1 = (gRandom->Uniform(0,1) < pfu1frac2) ? (1.+pfc1*fabs(z2))/pfn1*pfu1sigma2*z1+pfu1mean : (1.+pfc1*fabs(z2))/pfn1*pfu1sigma1*z1+pfu1mean;
-  Double_t pfu2 = (gRandom->Uniform(0,1) < pfu2frac2) ? (1.+pfc2*fabs(z1))/pfn2*pfu2sigma2*z2+pfu2mean : (1.+pfc2*fabs(z1))/pfn2*pfu2sigma1*z2+pfu2mean;
-
+  Double_t pfu1 = (gRandom->Uniform(0,1) < pfu1frac2) ? z1*pfu1sigma2+pfu1mean : z1*pfu1sigma1+pfu1mean;
+  Double_t pfu2 = (gRandom->Uniform(0,1) < pfu2frac2) ? z2*pfu2sigma2+pfu2mean : z2*pfu2sigma1+pfu2mean;
  
-  TVector2 vpfmet(-(dilepPt+pfu1)*cos(dilepPhi)+pfu2*sin(dilepPhi), -(dilepPt+pfu1)*sin(dilepPhi)-pfu2*cos(dilepPhi));  
+  TVector2 vpfmet(-pfu1*cos(genWPhi)+pfu2*sin(genWPhi)-lepPt*cos(lepPhi), -pfu1*sin(genWPhi)-pfu2*cos(genWPhi)-lepPt*sin(lepPhi));  
   pfmet    = vpfmet.Mod();
   pfmetphi = TVector2::Phi_mpi_pi(vpfmet.Phi());
-//*/
 }
 
