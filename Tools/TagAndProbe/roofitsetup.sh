a194d56fefa1429c8f53b6a9e86e02434baa504b
ksung <>
2012-06-04 11:26:05 +0000

    changed 5.32.00 to 5.28.00h

--- Tools/TagAndProbe/roofitsetup.sh
+++ Tools/TagAndProbe/roofitsetup.sh
@@ -4,5 +4,5 @@
 #. /afs/cern.ch/sw/lcg/app/releases/ROOT/5.27.06/x86_64-slc5-gcc43-opt/root/bin/thisroot.sh
 
 . /afs/cern.ch/sw/lcg/external/gcc/4.6.2/x86_64-slc5/setup.sh
-. /afs/cern.ch/sw/lcg/app/releases/ROOT/5.32.00/x86_64-slc5-gcc46-opt/root/bin/thisroot.sh
+. /afs/cern.ch/sw/lcg/app/releases/ROOT/5.28.00h/x86_64-slc5-gcc43-opt/root/bin/thisroot.sh
 
